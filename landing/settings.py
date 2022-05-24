b3765fd03eb1ed448eda6d5d2ad8bd34ef4bbd38
Yar417 <iaroslavnegodenko@gmail.com>
2022-05-24 17:06:32 +0300

    one more commit

--- landing/settings.py
+++ landing/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'price.apps.PriceConfig',
     'crm.apps.CrmConfig',
+    'cms.apps.CmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
